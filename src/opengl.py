a9a140cc588f1626635f553e0e64055ee07821d3
Molnár Botond <45335158+Gepsonka@users.noreply.github.com>
2024-09-16 23:05:15 +0200

    working triangle

--- src/opengl.py
+++ src/opengl.py
@@ -3,13 +3,18 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from OpenGL.GL import *
 
-WINDOW_SIZE = (1200, 1200)
+WINDOW_SIZE = (1200, 800)
 WINDOW_POSITION = (50, 50)
-WINDOW_TITLE = "Surfaces"
+WINDOW_TITLE = b"Surfaces"
 
 
 class Camera:
-    def __init__(self, coords: tuple[float, float, float], looking_at: tuple[float, float, float], up_vec: tuple[float, float, float]):
+    def __init__(
+        self,
+        coords: tuple[float, float, float],
+        looking_at: tuple[float, float, float],
+        up_vec: tuple[float, float, float],
+    ):
         self._coords = coords
         self._looking_at = looking_at
         self._up_vec = up_vec
@@ -21,7 +26,12 @@ class Camera:
 camera = Camera((25.0, 25.0, 50.0), (25.0, 25.0, 0.0), (0.0, 1.0, 0.0))
 
 
-def initGlut(displayFunc: Callable[[], None], resizeFunc: Callable[[int, int], None] | None = None, keyboardFunc: Callable[[int, int, int], None] | None = None, specialFunc: Callable[[int, int, int], None] | None = None) -> None:
+def initGlut(
+    displayFunc: Callable[[], None],
+    resizeFunc: Callable[[int, int], None] | None = None,
+    keyboardFunc: Callable[[int, int, int], None] | None = None,
+    specialFunc: Callable[[int, int, int], None] | None = None,
+) -> None:
     glutInit()
     # glutInitContextVersion(4, 1)
     # glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE)
@@ -39,16 +49,16 @@ def initGlut(displayFunc: Callable[[], None], resizeFunc: Callable[[int, int], N
     if glutSpecialFunc is not None:
         glutSpecialFunc(specialFunc)
 
-    glClearColor(1.0, 1.0, 1.0, 0.0)
+    glClearColor(0.0, 0.0, 0.0, 1.0)
 
     glutMainLoop()
 
 
 def resizeFunction(w, h):
-    glViewport(0,0, w, h)
+    glViewport(0, 0, w, h)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluPerspective(45.0, w/h, 0.1, 100.0)
+    gluPerspective(45.0, w / h, 0.1, 100.0)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
 
@@ -62,13 +72,13 @@ def displayFunction():
 
     camera.init_camera()
 
-    glBegin(GL_LINE_LOOP)
-
-    glColor3f(1.0, 1.0, 1.0)
-    glVertex3f(10.0, 10.0, 0.0)
+    glBegin(GL_TRIANGLES)
+    glColor3f(1.0, 0.0, 0.0)  # Red color
     glVertex3f(20.0, 20.0, 0.0)
-
-
+    glColor3f(0.0, 1.0, 0.0)  # Green color
+    glVertex3f(25.0, 30.0, 0.0)
+    glColor3f(0.0, 0.0, 1.0)  # Blue color
+    glVertex3f(40.0, 20.0, 0.0)
     glEnd()
 
-    glFlush()
+    glutSwapBuffers()
